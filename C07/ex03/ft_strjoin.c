7ccbe4fb004f458d934509a662783a5018014017
Antonio Jesus Canales Delgado <ancanale@c1r1s5.42malaga.com>
2025-02-14 13:36:13 +0100

    C07ex03

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -6,16 +6,37 @@
 /*   By: ancanale <ancanale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 14:53:54 by ancanale          #+#    #+#             */
-/*   Updated: 2025/02/13 18:21:27 by ancanale         ###   ########.fr       */
+/*   Updated: 2025/02/14 13:03:17 by ancanale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	j = 0;
+	while (dest[j])
+	{
+		j++;
+	}
+	i = 0;
+	while (src[i])
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
+
 int	ft_strlen(char *str)
 {
 	int	len;
-	
+
 	len = 0;
 	while (str[len])
 		len++;
@@ -39,22 +60,33 @@ int	calc_tlen(int size, char **strs, char *sep)
 		if (i < size - 1)
 			total_len += sep_len;
 	}
-	return total_len;
+	return (total_len);
 }
 
 char	*ft_strjoin(int size, char **strs, char *sep)
 {
 	char	*result;
 	int		total_len;
+	int		i;
 
 	if (size == 0)
 	{
-		result = (char*)malloc(size + 1);
+		result = (char *)malloc(size + 1);
 		result[0] = '\0';
 		return (result);
 	}
-	int total_len = calc_tlen(size, strs, sep);	
-	result = (char*)malloc(total_len + 1);
+	total_len = calc_tlen(size, strs, sep);
+	result = (char *)malloc(total_len + 1);
 	if (!result)
-		return NULL;
+		return (NULL);
+	i = 0;
+	result[0] = '\0';
+	while (i < size)
+	{
+		ft_strcat(result, strs[i]);
+		if (i < size - 1)
+			ft_strcat(result, sep);
+		i++;
+	}
+	return (result);
 }
