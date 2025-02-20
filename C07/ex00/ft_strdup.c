17bc264909c03bf422633f09cb8eb455c54e3453
Antonio Jesus Canales Delgado <ancanale@c1r4s2.42malaga.com>
2025-02-20 18:37:16 +0100

    from piscine split

--- C07/ex00/ft_strdup.c
+++ C07/ex00/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: ancanale <ancanale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 17:28:14 by ancanale          #+#    #+#             */
-/*   Updated: 2025/02/11 18:03:25 by ancanale         ###   ########.fr       */
+/*   Updated: 2025/02/19 18:21:08 by ancanale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,12 @@ char	*ft_strdup(char *src)
 	dup = malloc(sizeof(char) * ft_strlen(src) + 1);
 	if (!(dup))
 		return (NULL);
-	dup[ft_strlen(src)] = '\0';
 	i = 0;
 	while (src[i])
 	{
 		dup[i] = src[i];
 		i++;
 	}
+	dup[i] = '\0';
 	return (dup);
 }
