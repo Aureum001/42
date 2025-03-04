1cee614d82dd68e7bbaf9b5d03bf61fe0b95cba1
Aureum001 <antonioayr.94@gmail.com>
2025-03-04 14:36:11 +0100

    main part done TODO: Bonus + mains for defense testing

---
+++ libft/ft_split.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ancanale <antonioayr.94@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 09:50:45 by ancanale          #+#    #+#             */
+/*   Updated: 2025/03/04 09:50:45 by ancanale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+static int	count_words(char const *s, char c)
+{
+	int	count;
+
+	count = 0;
+	while (*s)
+	{
+		while (*s == c)
+			s++;
+		if (*s)
+			count++;
+		while (*s != c)
+			s++;
+	}
+	return (count);
+}
+
+static char	*allocate_word(char const *s, char c, char **strs)
+{
+	char	*word;
+	
+	word = ft_strchr(s, c);
+	if (!word || !c)
+	{
+		word = (char *)s;
+		while (*word)
+			word++;
+	}
+	*strs = ft_substr(s, 0, word - s);
+	return (word);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**strs;
+	char	**buf;
+
+	if (!s)
+		return (NULL);
+	strs = (char **)malloc((count_words(s, c) + 1) * sizeof(*strs));
+	if (!strs)
+		return (NULL);
+	buf = strs;
+	while (*s)
+	{
+		while (*s == c)
+			s++;
+		if (*s)
+			s = allocate_word(s, c, strs++);
+	}
+	*strs = 0;
+	return (buf);
+}
---
+++ libft/ft_strtrim.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ancanale <antonioayr.94@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 09:32:29 by ancanale          #+#    #+#             */
+/*   Updated: 2025/03/04 09:32:29 by ancanale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	len;
+
+	if (!s1 || !set)
+		return (NULL);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	len = ft_strlen(s1);
+	while (ft_strrchr(set, *(s1 + len - 1)))
+		len--;
+	return (ft_substr(s1, 0, len));
+}
