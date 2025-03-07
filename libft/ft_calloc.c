7ff553ae3b2e7d8b8efd345f9af244c9ba800616
Aureum001 <antonioayr.94@gmail.com>
2025-03-07 12:55:10 +0100

    changed < to " on lib include

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 void	*ft_calloc(size_t mem, size_t n)
 {
--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 static size_t	n_digits(long long n)
 {
