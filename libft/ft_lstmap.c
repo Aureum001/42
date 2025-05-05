50ef498bfc70bb022fbf4c5369e2304f68f41a40
Antonio Jesus Canales Delgado <ancanale@c3r6s5.42malaga.com>
2025-05-05 18:20:24 +0200

    changes to ft_lstmap.c

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -12,6 +12,19 @@
 
 #include "libft.h"
 
+static t_list	*create_mapped_node(void *(*f)(void *),
+					void *content, void (*del)(void *))
+{
+	void	*new_content;
+	t_list	*new_node;
+
+	new_content = f(content);
+	new_node = ft_lstnew(new_content);
+	if (!new_node)
+		del(new_content);
+	return (new_node);
+}
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*result;
@@ -19,18 +32,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 
 	if (!lst || !f)
 		return (NULL);
-	result = ft_lstnew(f(lst->content));
+	result = create_mapped_node(f, lst->content, del);
 	if (!result)
 		return (NULL);
 	head = result;
 	while (lst->next)
 	{
 		lst = lst->next;
-		result->next = ft_lstnew(f(lst->content));
+		result->next = create_mapped_node(f, lst->content, del);
 		if (!result->next)
 		{
 			ft_lstclear(&head, del);
-			break ;
+			return (NULL);
 		}
 		result = result->next;
 	}
