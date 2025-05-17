23a512a20e4cf9a53af49370d73297f13653bdb9
Aureum001 <antonioayr.94@gmail.com>
2025-05-17 23:41:27 +0200

    get_next_line with nodes for bonus first iter

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -10,3 +10,116 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+static char	*read_file(int fd, char *buffer)
+{
+	char	*read_buf;
+	int		bytes_read;
+
+	read_buf = malloc(BUFFER_SIZE + 1);
+	if (!read_buf)
+		return (NULL);
+	bytes_read = 1;
+	while (bytes_read > 0 && !ft_strchr(buffer, '\n'))
+	{
+		bytes_read = read(fd, read_buf, BUFFER_SIZE);
+		if (bytes_read < 0)
+		{
+			free(read_buf);
+			return (NULL);
+		}
+		read_buf[bytes_read] = '\0';
+		buffer = ft_strjoin(buffer, read_buf);
+		if (!buffer)
+			break;
+	}
+	free(read_buf);
+	return (buffer);
+}
+
+static char	*extract_line(char *buffer)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	if (!buffer || !buffer[0])
+		return (NULL);
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (buffer[i] == '\n')
+		i++;
+	line = malloc(i + 1);
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	if (buffer[i] == '\n')
+		line[i++] == '\n';
+	line[i] = '\0';
+	return (line);
+}
+
+static char *update_buffer(char *buffer)
+{
+	char	*new_buffer;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (!buffer[i])
+	{
+		free(buffer);
+		return (NULL);
+	}
+	i++;
+	new_buffer = malloc(ft_strlen(buffer) - i + 1);
+	if (!new_buffer)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	j = 0;
+	while (buffer[i])
+		new_buffer[j++] = buffer[i++];
+	new_buffer[j] = '\0';
+	free(buffer);
+	return (new_buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	static t_fd_list	*fd_list;
+	t_fd_list			*node;
+	char				*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		return (NULL);
+	node = find_fd_node(&fd_list, fd);
+	if (!node)
+	{
+		node = create_fd_node(fd);
+		if (!node)
+			return (NULL);
+		node->next = fd_list;
+		fd_list = node;
+	}
+	node->buffer = read_file(node->fd, node->buffer);
+	if (!node->buffer)
+	{
+		free_fd_node(&fd_list, fd);
+		return (NULL);
+	}
+	line = extract_line(node->buffer);
+	node->buffer = update_buffer(node->buffer);
+	if (!node->buffer)
+		free_fd_node(&fd_list, fd);
+	return (line);
+}
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -10,3 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "./libft/libft.h"
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+/* Structure to handle multiple file descriptors */
+typedef struct s_fd_list
+{
+	int					fd;
+	char				*buffer;
+	struct s_fd_list	*next;
+}	t_fd_list;
+
+/* Function prototypes */
+char		*get_next_line(int fd);
+t_fd_list	*find_fd_node(t_fd_list **list, int fd);
+t_fd_list	*create_fd_node(int fd);
+void		free_fd_node(t_fd_list **list, int fd);
+
+#endif
