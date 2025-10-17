a4bcdfa94a84687484691a3757d8967f034f6d52
Antonio Jesus Canales Delgado <ancanale@c2r1s5.42malaga.com>
2025-10-17 09:59:24 +0200

    fixed quote states for parsing

--- minishell/src/parser_utils.c
+++ minishell/src/parser_utils.c
@@ -6,7 +6,7 @@
 /*   By: ancanale <ancanale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 10:48:46 by ancanale          #+#    #+#             */
-/*   Updated: 2025/10/07 10:59:06 by ancanale         ###   ########.fr       */
+/*   Updated: 2025/10/17 09:57:35 by ancanale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,24 +28,70 @@ char	*expand_variables(char *str, char **envp, int last_status)
 	return (result);
 }
 
+static char	*process_quote_section(char *value, int *i,
+			char **envp, int last_status)
+{
+	char	quote;
+	int		start;
+	char	*section;
+	char	*expanded;
+
+	quote = value[*i];
+	start = ++(*i);
+	while (value[*i] && value[*i] != quote)
+		(*i)++;
+	section = ft_substr(value, start, *i - start);
+	if (value[*i] == quote)
+		(*i)++;
+	if (quote == '"')
+	{
+		expanded = expand_variables(section, envp, last_status);
+		free(section);
+		return (expanded);
+	}
+	return (section);
+}
+
+static char	*process_unquoted_section(char *value, int *i,
+				char **envp, int last_status)
+{
+	int		start;
+	char	*section;
+	char	*expanded;
+
+	start = *i;
+	while (value[*i] && value[*i] != '\'' && value[*i] != '"')
+		(*i)++;
+	section = ft_substr(value, start, *i - start);
+	expanded = expand_variables(section, envp, last_status);
+	free(section);
+	return (expanded);
+}
+
 char	*expand_and_remove_quotes(char *value, char **envp, int last_status)
 {
-	int		len;
-	char	*temp;
 	char	*result;
+	char	*section;
+	int		i;
 
 	if (!value)
 		return (NULL);
-	len = ft_strlen(value);
-	if (len >= 2 && value[0] == '\'' && value[len - 1] == '\'')
-		result = ft_substr(value, 1, len - 2);
-	else if (len >= 2 && value[0] == '"' && value[len - 1] == '"')
+	result = ft_strdup("");
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (value[i])
 	{
-		temp = ft_substr(value, 1, len - 2);
-		result = expand_variables(temp, envp, last_status);
-		free(temp);
+		if (value[i] == '\'' || value[i] == '"')
+			section = process_quote_section(value, &i, envp, last_status);
+		else
+			section = process_unquoted_section(value, &i, envp, last_status);
+		if (!section)
+			return (free(result), NULL);
+		result = ft_strjoin(result, section);
+		free(section);
+		if (!result)
+			return (NULL);
 	}
-	else
-		result = expand_variables(value, envp, last_status);
 	return (result);
 }
